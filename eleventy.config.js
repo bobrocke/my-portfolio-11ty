755a82dea6ca5495fa809e7ea2aef8698b916496
Bob Rockefeller <bob@bobrockefeller.com>
2026-01-11 16:35:54 -0500

    All the features are working

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,64 +1,103 @@
 import { VentoPlugin } from "eleventy-plugin-vento";
 import exifr from "exifr";
 import image from "@11ty/eleventy-img";
+import { DateTime } from "luxon";
 
 import fs from "fs";
 import path from "path";
 
+function getAspectRatio(imagePath) {
+  const metadata = image.statsSync(imagePath, {
+      widths: [null], // gets original width
+      formats: ["jpg"]
+  });
+
+  const width = metadata.jpeg[0].width;
+  const height = metadata.jpeg[0].height;
+
+  const ratio = (width / height);
+  let aspectRatio = ratio.toFixed(2);
+
+  return aspectRatio;
+}
+
+async function buildImageCollection(imageDirectory) {
+  const fileNames = fs.readdirSync(imageDirectory);
+  let jpgFiles = fileNames.filter(function(fileName) {
+    return fileName.endsWith('.jpg')
+  });
+
+  jpgFiles.sort(() => Math.random() - 0.5);
+
+  const images = await Promise.all(
+    jpgFiles.map(async function(image) {
+      const imageSource = path.join(".", imageDirectory, image);
+      const imagePath = path.join("/", imageDirectory, image);
+      const data = await exifr.parse(imageSource);
+      const imageDescription = data.ImageDescription;
+      // const imageDescription = data.ImageDescription;
+
+      return {
+        source: imagePath,
+        description: imageDescription,
+        aspectRatio: getAspectRatio(imageSource)
+      };
+    })
+  );
+
+  // console.log(images);
+
+  return images;
+}
+
 export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/css");
+  eleventyConfig.addPassthroughCopy("src/assets/js");
   eleventyConfig.addPassthroughCopy("src/assets/images");
+  eleventyConfig.addPassthroughCopy("src/assets/fonts");
   eleventyConfig.addPassthroughCopy("galleries");
-
-  eleventyConfig.addShortcode("getAspectRatio", async function (imagePath) {
-    const metadata = image.statsSync(imagePath, {
-        widths: [null], // gets original width
-        formats: ["jpg"]
-    });
-
-    const width = metadata.jpeg[0].width;
-    const height = metadata.jpeg[0].height;
-
-    const ratio = (width / height);
-    let aspectRatio = ratio.toFixed(2);
-
-    return aspectRatio;
-  });
-
-  eleventyConfig.addShortcode("getDescription", async function (imagePath) {
-    let description = await exifr.parse(imagePath, ['ImageDescription']);
-
-    return description;
-  });
 
   // Create a collection of all the fauna images
   eleventyConfig.addCollection("fauna", function(collectionApi) {
-    // const imageDirectory = path.resolve(__dirname, "assets/images/galleries/fauna");
-    const imageDirectory = "galleries/fauna";
-    const fileNames = fs.readdirSync(imageDirectory);
-    const jpgFiles = fileNames.filter(function(fileName) {
-      return fileName.endsWith('.jpg')
-    });
-    const images = jpgFiles.map(function(image) {
-      return path.join("/", imageDirectory, image)
-    });
+    const images = buildImageCollection("galleries/fauna");
 
     return images;
   });
 
   // Create a collection of all the flora images
   eleventyConfig.addCollection("flora", function(collectionApi) {
-    // const imageDirectory = path.resolve(__dirname, "assets/images/galleries/fauna");
-    const imageDirectory = "galleries/flora";
-    const fileNames = fs.readdirSync(imageDirectory);
-    const jpgFiles = fileNames.filter(function(fileName) {
-      return fileName.endsWith('.jpg')
-    });
-    const images = jpgFiles.map(function(image) {
-      return path.join("/", imageDirectory, image)
-    });
+    const images = buildImageCollection("galleries/flora");
 
     return images;
+  });
+
+  // Create a collection of all the people images
+  eleventyConfig.addCollection("people", function(collectionApi) {
+    const images = buildImageCollection("galleries/people");
+
+    return images;
+  });
+
+  // Create a collection of all the pets images
+  eleventyConfig.addCollection("pets", function(collectionApi) {
+    const images = buildImageCollection("galleries/pets");
+
+    return images;
+  });
+
+  // Create a collection of all the places images
+  eleventyConfig.addCollection("places", function(collectionApi) {
+    const images = buildImageCollection("galleries/places");
+
+    return images;
+  });
+
+  eleventyConfig.addFilter("date", (dateObj) => {
+    let thisDateTime = DateTime.fromJSDate(dateObj, { zone: "utc" }).setZone(
+      "America/New_York",
+      { keepLocalTime: true },
+    );
+    return thisDateTime.toLocaleString(DateTime.DATE_MED);
   });
 
   eleventyConfig.addPlugin(VentoPlugin, {
--- src/about.md
+++ src/about.md
@@ -1,8 +1,8 @@
 ---
-title: About
+title: The Photography of Bob Rockefeller
 layout: layouts/about.vto
 ---
 
-# Bob Rockefeller
+I capture images of the things that interest me most. That's why you'll see pictures from nature and of people, pets, and vacations here.
 
-Just some stuff.
+My primary photo gear is Olympus: OM-1 camera and the excellent 12-40mm f/2.8 II PRO, 40-150mm f/2.8 PRO, and the 300mm f4.0 IS PRO. At home, I print the best of my images with an Epson SureColor P800.
